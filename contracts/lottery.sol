072e8f3d7ebd9c90879b2bf43e2a673abde23141
seok <ikr@kakao.com>
2022-11-07 19:26:10 +0900

    Update lottery.sol
    WIP

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,6 +1,5 @@
-// SPDX-License-Identifier: MIT;
-
-pragma solidity >=0.7.0 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
 
 /**
  * @title Lottery
@@ -8,25 +7,36 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 contract Lottery {
     // 플레이어 관련
+    struct Player {
+        address _address;
+        uint256 _tickets;
+    }
+
     address public manager;
     address public lastWinner;
+
     address payable[] public players;
+    mapping(address => Player) public ticketHolders;
 
     // 티켓 관련
     uint256 public ticketsBought = 0; // 현재 유저가 몇개 샀는지
-    uint256 ticketPrice = .0001 ether; // 가격
-    uint64 ticketMax = 1000; // 최대 구입 가능
-    uint256 public lotteryId;
+    uint256 ticketPrice = .1 ether; // 가격
+    uint256 ticketMax = 1000; // 최대 구입 가능
+    uint256 public lotteryId; // 몇개의 티켓
+    uint256 public lotteryStart;
+    uint256 public lotteryDuration;
+    bool public lotteryEnded;
+    uint256 public contractBalance; // SC 잔고
 
-    constructor() {
-        manager = msg.sender;
-        lotteryId = 0;
-    }
+    // 이벤트
+    event TicketsBought(address indexed _from, uint256 _quantity);
+    event ResetLottery();
 
-    // buyTickets 으로만 살 수 있어야 하니까
-    fallback() external payable {
-        buyTickets();
-    }
+    // receive() external payable {
+    //     buyTickets();
+    // }
+
+    // Modifiers
 
     // 다 팔림 modifier
     modifier allTicketsSold() {
@@ -41,13 +51,36 @@ contract Lottery {
 
     modifier enoughMoney() {
         if (msg.value < ticketPrice) {
-            throw;
+            revert();
         } else {
             _;
         }
     }
 
+    // 로또 진행 중?
+    modifier lotteryOngoing() {
+        require(block.timestamp < lotteryStart + lotteryDuration);
+        _;
+    }
+
+    // 로또 끝?
+    modifier lotteryFinished() {
+        require(block.timestamp > lotteryStart + lotteryDuration);
+        _;
+    }
+
     /** ------------ Functions ------------ **/
+
+    constructor() {
+        manager = msg.sender;
+        lotteryId = 0;
+        lotteryStart = block.timestamp;
+        lotteryDuration = 24 hours;
+    }
+
+    fallback() external payable {
+        buyTickets();
+    }
 
     function enter() public payable enoughMoney {
         players.push(payable(msg.sender));
@@ -63,20 +96,37 @@ contract Lottery {
     }
 
     // 한번에 최대 20장은 살 수 있음
-    function buyTickets() public payable returns (bool) {
-        // require() ...
-        address buyer = msg.sender;
-        ticketsBought++;
+    function buyTickets() public payable lotteryOngoing returns (bool success) {
+        address payable buyer = payable(msg.sender);
+
+        ticketsBought += ticketHolders[msg.sender];
+
+        players.push(buyer);
+        contractBalance += msg.value;
+
+        // buyer.send(.1 ether);
+        emit TicketsBought(msg.sender, ticketHolders[msg.sender]);
+        return true;
     }
 
     function pickWinner() public restricted {
+        // TODO 어떻게 뽑을지
         uint256 randomIndex = random() % players.length;
         players[randomIndex].transfer(address(this).balance);
         lastWinner = players[randomIndex];
-        lotteryId++;
 
+        contractBalance = 0;
+        resetLottery();
         // players 초기화
         players = new address payable[](0);
+    }
+
+    function resetLottery() public lotteryFinished returns (bool success) {
+        lotteryEnded = false;
+        lotteryStart = block.timestamp;
+        lotteryDuration = 24 hours;
+        emit ResetLottery();
+        return true;
     }
 
     /** ------------ Getter ------------ **/
@@ -87,5 +137,9 @@ contract Lottery {
 
     function getPlayers() public view returns (address payable[] memory) {
         return players;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
