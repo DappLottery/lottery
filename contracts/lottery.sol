93a71402b243b6382601f20562d2a2e1affd940c
bknote71 <bknote71@gmail.com>
2022-11-08 18:47:04 +0900

    my code

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,5 +1,3 @@
-// SPDX-License-Identifier: MIT;
-
 pragma solidity >=0.7.0 <0.9.0;
 
 /**
@@ -11,6 +9,62 @@ contract Lottery {
     address public manager;
     address public lastWinner;
     address payable[] public players;
+
+    /////////////////////////////////////////////////////
+    // my code: 당첨자를 뽑는 코드
+    mapping(uint256 => uint256) randomNumbers;
+
+    uint256 peopleCount;
+
+    struct Person {
+        uint256 selectNumber;
+        address addr;
+    }
+
+    mapping(uint256 => Person) people;
+
+    function setRandomNumber() public {
+        // 랜덤 넘버: 4개, 2자리
+        uint256 randomNumber = random();
+
+        // 11 22 33 44
+        randomNumber = randomNumber % 100000000;
+        for (uint256 i = 0; i < 4; i++) {
+            randomNumbers[i] = randomNumber % 100;
+            randomNumber /= 100;
+        }
+    }
+
+    // function enter(): 이 때 참여하는 사람이 랜덤 넘버를 선택하고, (사람주소: 랜덤넘버)를 매핑??
+    function myEnter() public {
+        // 참여자는 enter 시에 8자리 번호를 넘긴다.
+        uint256 selectNum;
+        people[peopleCount++] = Person(selectNum, msg.sender);
+    }
+
+    function myPickWinner() public restricted {
+        uint256 matchCount = 0;
+        for (uint256 i = 0; i < peopleCount; i++) {
+            uint256 selectNumber = people[i].selectNumber;
+            address pAddr = people[i].addr;
+
+            for (uint256 j = 0; j < 4; j++) {
+                if (randomNumbers[j] == selectNumber % 100) matchCount++;
+                selectNumber /= 100;
+            }
+
+            // match count에 따라서 차등 지급
+            if (matchCount == 1) {}
+            if (matchCount == 2) {}
+            if (matchCount == 3) {}
+            if (matchCount == 4) {}
+            // <---------------------------------->
+        }
+        // people 초기화.
+        peopleCount = 0;
+    }
+
+    ///////////////////////////////////////////////////////////////////////////
 
     // 티켓 관련
     uint256 public ticketsBought = 0; // 현재 유저가 몇개 샀는지
@@ -40,9 +94,7 @@ contract Lottery {
     }
 
     modifier enoughMoney() {
-        if (msg.value < ticketPrice) {
-            throw;
-        } else {
+        if (msg.value < ticketPrice) {} else {
             _;
         }
     }
