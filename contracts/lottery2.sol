b8cfa3b6d29f4f6072cfb7804ad9694f54f02f24
seok <ikr@kakao.com>
2022-11-18 22:39:36 +0900

    Update lottery2.sol
    WIP
    
    transfer 테스트 너무 빡셈

--- contracts/lottery2.sol
+++ contracts/lottery2.sol
@@ -6,16 +6,15 @@ pragma solidity ^0.8.0;
  * @dev Lottery 스마트 컨트랙트
  */
 contract Lottery2 {
-    // ticket struct
+    // 티켓 관련
     struct Ticket {
         uint256 id;
-        uint8[] number;
+        uint8[6] number;
     }
 
     // 플레이어 관련
     struct Player {
         address payable addr;
-        Ticket[] boughtTickets;
         Ticket[] firstTickets;
         Ticket[] secondTickets;
         Ticket[] thirdTickets;
@@ -35,7 +34,7 @@ contract Lottery2 {
 
     // mapping(address => Player) public players;
     Player[] public players; // 모든 플레이어 돌 때 용
-    mapping(address => Player) public playersMap; // 플레이어 찾을 때 (gas fee 위해)
+    mapping(address => Ticket[]) public ticketMap; // 플레이어 찾을 때 (gas fee 위해)
     mapping(address => bool) public playerChecks; // solidity mapping existence 개념 없음
 
     uint256 actualPrice = 0;
@@ -95,22 +94,20 @@ contract Lottery2 {
 
     /** ------------ Functions ------------ **/
 
-    constructor() public {
+    constructor() public payable {
         owner = msg.sender;
         lotteryId = 0;
     }
 
     // 주소로 바로 돈 보낼 때
-    fallback() external payable {
-        buyTickets();
-    }
+    // fallback() external payable {
+    //     buyTickets();
+    // }
 
     function random() private view returns (uint256) {
         return
             uint256(
-                keccak256(
-                    abi.encodePacked(block.difficulty, block.timestamp, players)
-                )
+                keccak256(abi.encodePacked(block.difficulty, block.timestamp))
             );
     }
 
@@ -125,41 +122,37 @@ contract Lottery2 {
 
     // 일반적 절차: BUY - PICK - SEND
     // 한번에 최대 20장은 살 수 있음
+    // dApp에서 넘버 생성 누르면 번호 뜨고 그 옆에 몇 장 살껀지도
     // lotteryOngoing
-    function buyTickets(uint256 amount)
-        public
-        payable
-        restricted
-        returns (bool success)
-    {
-        Player player;
-        if (playerChecks[msg.sender] == false) {
-            player = Player(payable(msg.sender), [], []);
-            playerChecks[msg.sender] = true;
-        } else {
-            // 없으면 일단 entry 넣음
-            // TODO array check
-        }
+    function buyTickets() external payable returns (bool success) {
+        // if (msg.value < amount * ticketPrice) {
+        //     return false; // 또는 돈 만큼 티켓 사주기
+        // }
 
-        if (msg.value < amount * ticketPrice) {
-            return false; // 또는 돈 만큼 티켓 사주기
-        }
+        payable(msg.sender).transfer(msg.value);
 
-        address payable buyer = payable(msg.sender);
+        playerChecks[msg.sender] = true;
+
+        uint8[6] memory number = [1, 2, 3, 4, 5, 6];
+        ticketMap[msg.sender].push(Ticket(random(), number));
+
         contractBalance += msg.value;
-        lotteryId++; // dApp 에서 로또 총 몇개 팔렸는지 쉽게
-
-        emit TicketsBought(msg.sender, amount);
+        lotteryId++; // dApp 에서 로또 총 몇개 팔렸는지 쉽게        emit TicketsBought(msg.sender, amount);
         return true;
     }
 
     function pickWinner() public enoughLottery restricted {
-        for (uint256 i = 0; i < players.length; i++) {
-            Player player = players[i];
-            if (player.boughtTickets.length == 0) continue;
+        // 플레이어 없음
+        if (lotteryId == 0) {
+            revert();
+        }
 
-            for (uint256 j = 0; j < player.boughtTickets.length; j++) {
-                Ticket ticket = player.boughtTickets[j];
+        for (uint256 i = 0; i < players.length; i++) {
+            Player storage player = players[i];
+            if (ticketMap[player.addr].length == 0) continue;
+
+            for (uint256 j = 0; j < ticketMap[player.addr].length; j++) {
+                Ticket memory ticket = ticketMap[player.addr][j];
                 uint8 counts = 0;
 
                 for (uint8 t = 0; t < 6; t++) {
@@ -169,109 +162,86 @@ contract Lottery2 {
                 if (counts == 6) {
                     firstWinner++;
                     player.firstTickets.push(ticket);
+                } else if (counts >= 4) {
+                    // 차피 6은 포함 안된 if
+                    secondWinner++;
+                    player.secondTickets.push(ticket);
+                } else if (counts >= 2) {
+                    thirdWinner++;
+                    player.thirdTickets.push(ticket);
                 }
             }
         }
     }
 
-    // SEOK 당첨 관련
-    function divideMoney() public enoughLottery {
-        realTotalMoney = (contractBalance / 100) * 75; // 75%, fixed point도 없음;
+    function sendMoney() private restricted enoughLottery {
+        actualPrice = (contractBalance / 100) * cutRate; // 75%, fixed point도 없음;
 
-        uint256 firstMoneyToSend = (realTotalMoney / 100) * 70;
-        lastFirstMoney = firstMoneyToSend / (firstRanks);
+        if (firstWinner == 0) {
+            nextGameBalance += (actualPrice / 100) * 70;
+        } else {
+            firstMoney = ((actualPrice / 100) * 70) / firstWinner;
+        }
 
-        uint256 secondMoneyToSend = (realTotalMoney / 100) * 15;
-        lastSecondMoney = secondMoneyToSend / (secondRanks);
+        if (secondWinner == 0) {
+            nextGameBalance += (actualPrice / 100) * 15;
+        } else {
+            secondMoney = ((actualPrice / 100) * 15) / secondWinner;
+        }
 
-        uint256 thirdMoneyToSend = (realTotalMoney / 100) * 15;
-        lastThirdMoney = thirdMoneyToSend / (thirdRanks);
+        if (thirdWinner == 0) {
+            nextGameBalance += (actualPrice / 100) * 15;
+        } else {
+            thirdMoney = ((actualPrice / 100) * 15) / thirdWinner;
+        }
 
-        contractBalance -= realTotalMoney;
+        for (uint256 i = 0; i < players.length; i++) {
+            Player storage player = players[i];
+            eventMoney += player.firstTickets.length * firstMoney;
+            player.addr.transfer(player.firstTickets.length * firstMoney);
 
-        // players 들 loop 해서
-        // nMoneyToSend 씩 주면 됨
-        // ex) 1등 loop -> firstMoneyToSend 씩 주면 firstRanks 만큼의 돈이 나감
+            eventMoney += player.secondTickets.length * secondMoney;
+            player.addr.transfer(player.secondTickets.length * secondMoney);
 
-        // realTotalMoney 보여주기 위해 초기화 안함
+            eventMoney += player.thirdTickets.length * thirdMoney;
+            player.addr.transfer(player.thirdTickets.length * thirdMoney);
+        }
+
+        resetLottery();
     }
 
-    // SEOK 당첨 관련
-
-    function transferToWinner() public {
-        // 4개 맞춘 사람: 40%
-        // 3개 맞춘 사람: 30%
-        // 2개 맞춘 사람: 20%
-        // 1개 맞춘 사람: 10%
-        // uint256 _1 = (contractBalance / 10);
-        // uint256 _2 = ((2 * contractBalance) / 10);
-        // uint256 _3 = ((3 * contractBalance) / 10);
-        // uint256 _4 = ((4 * contractBalance) / 10);
-        // for (uint256 i = 0; i < winnerCount; i++) {
-        //     address payable pAddr = winners[i].addr;
-        //     uint256 m = winners[i].matchCount;
-        //     if (m == 1) {
-        //         transfer(pAddr, _1);
-        //     }
-        //     if (m == 2) {
-        //         transfer(pAddr, _2);
-        //     }
-        //     if (m == 3) {
-        //         transfer(pAddr, _3);
-        //     }
-        //     if (m == 4) {
-        //         transfer(pAddr, _4);
-        //     }
-        // }
-    }
-
-    function transfer(address payable _to, uint256 _amount) public {
-        _to.transfer(_amount);
-    }
-
-    function resetLottery() public lotteryFinished returns (bool success) {
-        lotteryEnded = false;
-        lotteryStart = block.timestamp;
-        lotteryDuration = 24 hours;
+    // lotteryFinished
+    function resetLottery() public returns (bool success) {
+        // lotteryEnded = false;
+        // lotteryStart = block.timestamp;
+        // lotteryDuration = 24 hours;
         emit ResetLottery();
         return true;
     }
 
     /** ------------ Getter ------------ **/
 
-    function getWinner() public view returns (address) {
-        return lastWinner;
-    }
+    // function getWinner() public view returns (address) {
+    //     return lastWinner;
+    // }
 
-    function getPlayers() public view returns (address payable[] memory) {
+    function getPlayers() public view returns (Player[] memory) {
         return players;
     }
 
-    function getBalance() public view returns (uint256) {
-        return address(this).balance;
-    }
-
-    function getFirstPlace() public view returns (uint256) {
-        return firstRanks;
-    }
-
-    function getSecondPlace() public view returns (uint256) {
-        return secondRanks;
-    }
-
-    function getThirdPlace() public view returns (uint256) {
-        return thirdRanks;
+    function getWinMoney() public view returns (uint256) {
+        return contractBalance;
     }
 
     function getFirstMoney() public view returns (uint256) {
-        return lastFirstMoney;
+        return firstMoney;
     }
 
     function getSecondMoney() public view returns (uint256) {
-        return lastSecondMoney;
+        return secondMoney;
     }
 
     function getThirdMoney() public view returns (uint256) {
-        return lastThirdMoney;
+        return thirdMoney;
     }
 }
