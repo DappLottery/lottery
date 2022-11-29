7c3c277725a56d0dc7ab87a45cd4aaf94444e83b
leehansik <116805256+leehansik@users.noreply.github.com>
2022-11-29 23:39:45 +0900

    Update TicketList.svelte
    ticket list

--- frontend/src/components/LotteryInfo/TicketList.svelte
+++ frontend/src/components/LotteryInfo/TicketList.svelte
@@ -17,7 +17,40 @@
 </script>
 
 <div>
-
+  {#await fetchData()}
+    Fetching contract dataset_dev...
+  {:then _}
+    <br />
+    List of mytickets
+    <br />
+    <br />
+    <div class="ticket-info-list">
+      <table
+        border="1"
+        bordercolor="white"
+        width="700"
+        height="100"
+        align="center"
+      >
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Ticket ID</th>
+            <th>Ticket Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {#each myTickets as myTicket, i}
+              <th scope="row">{i}</th>
+              <td>{myTicket.id}</td>
+              <td>{myTicket.number}</td>
+            {/each}
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  {/await}
 </div>
 
 <style>
