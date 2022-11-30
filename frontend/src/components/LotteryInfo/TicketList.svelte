8a0b331e35164baa8f2fcefb07df99a575992648
RootLEE2 <2rootcom4@gmail.com>
2022-11-30 14:55:28 +0900

    feat: ticket list component

--- frontend/src/components/LotteryInfo/TicketList.svelte
+++ frontend/src/components/LotteryInfo/TicketList.svelte
@@ -1,23 +1,38 @@
 <script>
-  export let selectedAccount;
-  export let contracts;
-
-  let myTickets;
-
-  const fetchData = async () => {
-    try {
-      myTickets = await $contracts.Lottery.methods
-        .getTickets($selectedAccount)
-        .call();
-    } catch (err) {
-      console.log(err);
-      throw new Error(err);
-    }
-  };
+  export let myTickets;
 </script>
 
 <div>
-
+  <br />
+  List of mytickets
+  <br />
+  <br />
+  <div class="ticket-info-list">
+    <table
+      border="1"
+      bordercolor="white"
+      width="700"
+      height="100"
+      align="center"
+    >
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Ticket ID</th>
+          <th>Ticket Number</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each myTickets as myTicket, i}
+        <tr>
+            <th scope="row">{i+1}</th>
+            <td>{myTicket.id}</td>
+            <td>{myTicket.number}</td>
+        </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </div>
 
 <style>
