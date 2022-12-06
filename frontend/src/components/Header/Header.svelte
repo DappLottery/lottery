880df5588f41d231326e39b7d8834fa0557432b3
RootLEE2 <2rootcom4@gmail.com>
2022-12-06 22:58:52 +0900

    feat: header component

---
+++ frontend/src/components/Header/Header.svelte
@@ -0,0 +1,45 @@
+<script>
+
+</script>
+
+<header>
+	<div class="account">
+		<!-- <Account /> -->
+	</div>
+
+	<div class="title">
+		<span>Title</span>
+	</div>
+
+	<div class="menu">
+		<!-- <Menu /> -->
+	</div>
+</header>
+
+<style>
+	header {
+		position: relative;
+	}
+
+	.account,
+	.title,
+	.menu {
+		position: absolute;
+	}
+
+	.account {
+		left: 0%;
+		padding-left: 30px;
+	}
+
+	.title {
+		left: 50%;
+		transform: translate(-50%, 0);
+	}
+	
+	.menu {
+		right: 0%;
+		transform: translate(-100%, 0);
+		/* padding-right: 10px; */
+	}
+</style>
