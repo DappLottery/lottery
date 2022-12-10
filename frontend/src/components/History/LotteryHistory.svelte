5bdd21756e87f3287d4495ae3ad28fba1eedf3b1
seok <ikr@kakao.com>
2022-12-10 18:08:43 +0900

    Create a example function to fetch lottery history
    No schema yet

--- frontend/src/components/History/LotteryHistory.svelte
+++ frontend/src/components/History/LotteryHistory.svelte
@@ -1,3 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let lotteryId;
+
+  let games = [];
+
+  onMount(async () => {
+    await fetch(
+      "http://ec2-3-39-168-175.ap-northeast-2.compute.amazonaws.com:8010/history",
+      {
+        method: "GET",
+      }
+    )
+      .then(res => res.json())
+      .then(result => (games = result));
+
+    console.log(games);
+  });
+</script>
+
+<ul>
+    {#each games as game}
+      <li>
+        {game.id} | {game.winner}
+      </li>
+    {/each}
+</ul>
+
 <div class="lotto-hist">
   <br/>
   <div>scrollable list using DB</div>
