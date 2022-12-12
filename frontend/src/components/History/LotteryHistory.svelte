aab10c155dfcf5f8272999e8b65f4fb4c283b208
RootLEE2 <2rootcom4@gmail.com>
2022-12-12 09:59:10 +0900

    feat: navigator list for lottery history

--- frontend/src/components/History/LotteryHistory.svelte
+++ frontend/src/components/History/LotteryHistory.svelte
@@ -2,8 +2,9 @@
 	import { web3 } from "svelte-web3";
   import SvelteTable from 'svelte-table';
 	import { onMount } from "svelte";
+	import { useNavigate } from "svelte-navigator";
 
-  let rows = [];
+  let myTickets = [];
   onMount(async () => {
     await fetch(
       "http://ec2-3-39-168-175.ap-northeast-2.compute.amazonaws.com:8010/history",
@@ -13,45 +14,45 @@
     )
       .then(res => res.json())
       .then(result => {
-        rows = result;
-        rows.forEach((object, i) => {
-          object.id = i;
-        });
+        myTickets = result;
+        // rows.forEach((object, i) => {
+        //   object.id = i;
+        // });
       });
 
-    console.log(rows);
+    console.log(myTickets);
   });
 
-	// define column configs
-	const columns = [
-		{
-			key: "LotteryID",
-			title: "LotteryID",
-			value: v => v.id,
-			sortable: true,
-		},
-		{
-			key: "TicketSold",
-			title: "TicketSold",
-			value: v => v.ticket_sold,
-			sortable: true,
-		},
-		{
-			key: "GameMoney",
-			title: "GameMoney",
-			value: v => $web3.utils.fromWei(v.game_money.toString(), "ether"),
-			sortable: true,
-		},
-		{
-			key: "LuckyNumber",
-			title: "LuckyNumber",
-			value: v => v.lucky_number,
-			sortable: true,
-		},
-	];
+	const navigate = useNavigate();
 </script>
 
-<SvelteTable columns="{columns}" rows="{rows}"></SvelteTable>
+<h2>Lottery History</h2>
+<table
+	border="1"
+	bordercolor="white"
+	width="700"
+	height="100"
+	align="center"
+>
+	<thead>
+		<tr>
+			<th>LotteryID</th>
+			<th>TicketSold</th>
+			<th>GameMoney</th>
+			<th>LuckyNumber</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each myTickets as myTicket, i}
+			<tr>
+				<th scope="row" on:click={() => navigate(`${i}`)}>{i + 1}</th>
+				<td>{myTicket.ticket_sold}</td>
+				<td>{myTicket.game_money}</td>
+				<td>{myTicket.lucky_number}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
 
 <style>
 </style>
